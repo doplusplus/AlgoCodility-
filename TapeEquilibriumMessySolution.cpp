ee1241c80e7d12f8e6243cd5d45fe062d3b336dc
Doz <doraneplus@gmail.com>
2015-10-23 20:14:57 +0100

    Create TapeEquilibriumMessySolution.cpp

---
+++ TapeEquilibriumMessySolution.cpp
@@ -0,0 +1,29 @@
+
+#include<cmath>
+
+int solution(vector<int> &A) {
+    // write your code in C++11
+    
+    
+    long ref=0;
+    for (auto s=1;s<A.size();s++)
+        {   
+            ref += A[s];
+        }
+        
+    long diff = A[0] - ref;
+    ref = abs(diff);
+    
+    for (auto s=1;s<A.size()-1;s++)
+        {   
+            diff  += 2 * A[s];
+            
+            if(abs(diff) < ref) {ref = abs(diff);}    
+        }
+    
+    return ref;
+    
+}  
+
+
+/* Basically the performances are the same as the clear one only the number of variables differs  */
