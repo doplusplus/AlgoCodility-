74dc29f16bfa1198e53c6cb48fafb2fed41e08e7
Doz <doraneplus@gmail.com>
2016-06-27 12:44:06 +0100

    Update Distinct.cpp

--- Painless/Distinct.cpp
+++ Painless/Distinct.cpp
@@ -5,7 +5,7 @@ int solution(vector<int> &A) {
     if(n==0) return 0;
     
     int count=1;
-    sort(A.begin(),A.end());
+    sort(A.begin(),A.end()); //nLog(n)
     for(int i =0;i<n-1;i++)
     {
         if(A[i]!=A[i+1]){count++;}    
