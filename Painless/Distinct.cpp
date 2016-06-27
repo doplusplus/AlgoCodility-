cf33295c431f6b65fc6db6d95310ebff2e1cbcf1
Doz <doraneplus@gmail.com>
2016-06-27 12:39:47 +0100

    Create Distinct.cpp

---
+++ Painless/Distinct.cpp
@@ -0,0 +1,14 @@
+int solution(vector<int> &A) {
+    // write your code in C++11 (g++ 4.8.2)
+    
+    int n=A.size();
+    if(n==0) return 0;
+    
+    int count=1;
+    sort(A.begin(),A.end());
+    for(int i =0;i<n-1;i++)
+    {
+        if(A[i]!=A[i+1]){count++;}    
+    }
+    return count;
+}
