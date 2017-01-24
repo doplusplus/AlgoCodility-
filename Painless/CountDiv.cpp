db79a71be3d06a34d1fc6da0835e823f94287891
Doz <doraneplus@gmail.com>
2017-01-24 22:33:05 +0400

    Update CountDiv.cpp

--- Painless/CountDiv.cpp
+++ Painless/CountDiv.cpp
@@ -9,3 +9,12 @@ int solution(int A, int B, int K) {
     
     return div;
 }
+
+//other solution
+
+int solution(int A, int B, int K) {
+    // write your code in C++14 (g++ 6.2.0)
+    int res= B/K - A/K;
+    if(A%K==0) res++;
+    return res;
+}
